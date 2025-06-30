fcfa52dcdf665567052dbff1d9234e181338eca3
talitasilvad <talitasilvadee@gmail.com>
2025-06-30 15:52:05 -0300

    ajuste navegação com react router dom

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,5 @@
 import styles from "./Section.module.css"
+import { Link } from 'react-router-dom';
 
 const Section = ({title, titleAlign = "left", link, children}) => {
     const alignText = titleAlign === "center" ? styles.center : styles.left
@@ -8,8 +9,9 @@ const Section = ({title, titleAlign = "left", link, children}) => {
                <div className={`${styles.divSection} ${alignText}`}>
                   <h4 className={styles.title}>{title}</h4>
                   {link && (
-                    <a className={styles.link} href={link.href}>{link.text} 
-                    <i className="pi pi-arrow-right" style={{ marginLeft: '1rem' }}></i></a>
+                    <Link className={styles.link} to={link.href}>{link.text} 
+                    <i className="pi pi-arrow-right" style={{ marginLeft: '1rem' }}></i>
+                    </Link>
                   )}
                </div>
                <div>
